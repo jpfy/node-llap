e70f196eef6ddf6fcbaefc9356427c2607e4ef80
flabbergast <s3+flabbergast@sdfeu.org>
2013-04-03 08:30:23 +0200

    Incoming serial data is llap-packetized.

--- modules/serial_node.js
+++ modules/serial_node.js
@@ -87,10 +87,21 @@ var detectSerialOnRaspberryPI = function()
 	});
 }
 
+var incomingData = "";
+var llapParser = function(emitter, buffer){
+	incomingData += buffer.toString();
+	// remove any stuff before a signature 'a' appears
+	incomingData = incomingData.replace(/^[^]*?a/,'a');
+	while (incomingData.length >= 12) {
+		emitter.emit('data',incomingData.substr(0,12));
+		incomingData = incomingData.substr(12).replace(/^[^]*?a/,'a');
+	}
+}
+
 var attemptConnection = function(port)
 {
 	console.log('* attempting to connect to serial at :', port, ' *');
-	serport = new serialport.SerialPort(port, { baudrate: 9600 });
+	serport = new serialport.SerialPort(port, { baudrate: 9600, parser: llapParser });
 	serport.on("open", function () {
 		console.log('* connection to a serial port successful ! *');
 		serport.on('data', function(data){
